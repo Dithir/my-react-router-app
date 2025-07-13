9bdae25cace91bb7c2d56baebabd77c5dd0accac
Dithir <agustinbaez94@gmail.com>
2025-07-12 21:31:46 -0300

    Trying to deploy
    uwu

--- vite.config.ts
+++ vite.config.ts
@@ -5,5 +5,5 @@ import tsconfigPaths from "vite-tsconfig-paths";
 
 export default defineConfig({
   plugins: [tailwindcss(), reactRouter(), tsconfigPaths()],
-  base:"/my-react-router-app"
+  base:"/my-react-router-app/app"
 });
